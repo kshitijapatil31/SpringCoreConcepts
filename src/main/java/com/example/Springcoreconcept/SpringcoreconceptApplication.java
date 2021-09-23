a4c568cf411148a6d3ce5226e2fae6489424bcb8
Kshitija Patil <patilkshitija2@gmail.com>
2021-09-23 21:24:40 +0530

    [kshitija]:initial setup is added

---
+++ src/main/java/com/example/Springcoreconcept/SpringcoreconceptApplication.java
@@ -0,0 +1,13 @@
+package com.example.Springcoreconcept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcoreconceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoreconceptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Springcoreconcept/SpringcoreconceptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Springcoreconcept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoreconceptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
